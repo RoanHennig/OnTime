276321a2f9f31e5f8e4515914080927189c3bc65
RoanHennig <roanhennig@capitecbank.co.za>
2019-09-23 10:26:48 +0200

    First Checkin

---
+++ src/app/main/example/ExampleConfig.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export const ExampleConfig = {
+    settings: {
+        layout: {
+            config: {}
+        }
+    },
+    routes  : [
+        {
+            path     : '/example',
+            component: React.lazy(() => import('./Example'))
+        }
+    ]
+};
+
+
+/**
+ * Lazy load Example
+ */
+/*
+
+import Example from './Example';
+
+export const ExampleConfig = {
+    settings: {
+        layout: {
+            config: {}
+        }
+    },
+    routes  : [
+        {
+            path     : '/example',
+            component: Example
+        }
+    ]
+};
+*/
