63d61ac36bb672e6468603b9ce0dfc8bcd144b60
RoanHennig <roanhennig@capitecbank.co.za>
2019-09-24 10:12:46 +0200

    Addin

--- src/app/App.js
+++ src/app/App.js
@@ -10,7 +10,8 @@ import AppContext from './AppContext';
 import routes from './fuse-configs/routesConfig';
 import {create} from 'jss';
 import {StylesProvider, jssPreset, createGenerateClassName} from '@material-ui/styles';
-
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
 const jss = create({
     ...jssPreset(),
     plugins       : [...jssPreset().plugins, jssExtend()],
