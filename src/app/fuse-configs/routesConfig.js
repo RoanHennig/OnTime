51869d8931d6a60f91c9ae3532bb5da9d886d4e1
RoanHennig <roanhennig@capitecbank.co.za>
2019-09-27 06:17:47 +0200

    WIP

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -2,16 +2,17 @@ import React from 'react';
 import {Redirect} from 'react-router-dom';
 import {FuseUtils} from '@fuse';
 import {ExampleConfig} from 'app/main/example/ExampleConfig';
+import {authRoles} from 'app/auth';
 
 const routeConfigs = [
     ExampleConfig
 ];
 
 const routes = [
-    ...FuseUtils.generateRoutesFromConfigs(routeConfigs),
+    ...FuseUtils.generateRoutesFromConfigs(routeConfigs,authRoles.user),
     {
         path     : '/',
-        component: () => <Redirect to="/example"/>
+        component: () => <Redirect to="/login"/>
     }
 ];
 
--- src/app/main/example/ExampleConfig.js
+++ src/app/main/example/ExampleConfig.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export const ExampleConfig = {
     settings: {
         layout: {
